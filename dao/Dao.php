079a93dd5a6b3d8c1e87e98445542303654d1cac
sidney_carlos <bwb.carlos.s@fondespierre.com>
2021-06-07 14:30:34 +0000

    initialisation DAO

--- dao/Dao.php
+++ dao/Dao.php
@@ -1,43 +1,10 @@
 <?php
 
-abstract class Dao {
+abstract class Dao implements CRUDInterface, RepositoryInterface {
     protected $pdo;
 
-    public funtion __construct() {
-        $this->pdo = new Pdo();
+    public function __construct() {
+        $this->pdo = new PDO("");
     }
-    /**
-     * $id
-     * return entité
-     */
-    abstract public retrieve($id);
-
-    /**
-     * $id
-     * return bool
-     */
-    abstract public update($id);
-
-    /**
-     * $id
-     * return bool
-     */
-    abstract public delete($id);
-
-    /**
-     * $array: tableau associatif
-     * return entité
-     */
-    abstract public create($array);
-
-    /**
-     * return toute les valeurs d'une table
-     */
-    abstract public getAll();
-
-    /**
-     * $array : tableau associatif
-     * return valeur du tableau choisie dans le tableau où la value = une valeur déterminée
-     */
-    abstract public getAllBy($array);
+   
 }
--- dao/RepositoryInterface.php
+++ dao/RepositoryInterface.php
@@ -1,3 +1,16 @@
 <?php
-getAll();
-getAllBy($array);
+
+interface RepositoryInterface {
+    
+    /**
+     * @param array
+     * @return valeurs d'une table
+     */
+    public function getAll() : array;
+
+    /**
+     * @param array : tableau associatif
+     * @return valeurs du tableau choisies dans le tableau où la value = une valeur déterminée
+     */
+    public function getAllBy(array $args) : array;
+}
