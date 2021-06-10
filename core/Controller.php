78075dfb718d5e3f181d89358dcf3efe99b4874e
sidney_carlos <bwb.carlos.s@fondespierre.com>
2021-06-10 10:34:50 +0000

    prise en charge des inputs post et get

--- core/Controller.php
+++ core/Controller.php
@@ -1,14 +1,32 @@
 <?php
 
 abstract class Controller {
-    private $post;
-    private $get;
+    private $post; //propriété qui va stocker la superglobal post
+    private $get; //propriété qui va stocker la superglobal get
 
+    /**
+     * initialisation des propriétés post et get
+     */
     public function __construct() {
         $this->post = $_POST;
         $this->get = $_GET;
     }
 
+    /**
+     * est final pour qu'elle soit utilisée par le dev mais ne pourra pas etre modifiée
+     * @return superglobale get 
+     */
+    protected final function inputGet(){
+        return $this->get;
+    }
+
+     /**
+     * est final pour qu'elle soit utilisée par le dev mais ne pourra pas etre modifiée
+     * @return superglobale post 
+     */
+    protected final function inputPost(){
+        return $this->post;
+    }
     protected function render(string $viewPath, array $data) {
 
     }
